01cbe25bb0291e0e224d4a9c40ec472146fbadcb
Naveen <Naveen@Naveens-MacBook-Pro-2.local>
2024-03-02 09:12:12 +0000

    First Example

---
+++ SeleniumExample/src/selenium/examples/Example1.java
@@ -0,0 +1,16 @@
+package selenium.examples;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Example1 
+{
+    public static void main(String[] args) 
+    {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://selenium.dev");
+
+        //driver.quit();
+    }
+}
